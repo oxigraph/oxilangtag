d599a51f77ef18abd2d976c88c7007e4c9b166c8
Thomas Tanon <thomas@pellissier-tanon.fr>
2026-06-11 21:39:23 +0200

    Fuzzer: check case insensitivity

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -5,6 +5,12 @@ use std::str;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = str::from_utf8(data) {
-        let _ = LanguageTag::parse(s);
+        let parsed = LanguageTag::parse(s).ok();
+        let normalized = LanguageTag::parse_and_normalize(s).ok();
+        let normalized_upper = LanguageTag::parse_and_normalize(&s.to_ascii_uppercase()).ok();
+        let normalized_lower = LanguageTag::parse_and_normalize(&s.to_ascii_lowercase()).ok();
+        assert_eq!(parsed.is_some(), normalized.is_some());
+        assert_eq!(normalized, normalized_upper);
+        assert_eq!(normalized, normalized_lower);
     }
 });
